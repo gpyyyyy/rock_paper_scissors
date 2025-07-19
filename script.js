c309c93d93f6f0307ff287e96bce136dbf5fcf28
gpyyyyy <renaldo.steinbach079@gmail.com>
2025-07-19 19:00:34 +0200

    Created function for getHumanChoice

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+
+
 function getComputerChoice () {
     const min = 1;
     const max = 3;
     const randomChoice = Math.floor(Math.random() * (max - min + 1)) + min;
-    return randomChoice;
+        if (randomChoice == 1) {
+            return "rock"
+        } else if (randomChoice == 2) {
+            return "scissor"
+        } else {
+            return "paper"
+        }   
 }
 
-console.log(getComputerChoice()); 
+function getHumanChoice () {
+    const choice = prompt("Rock, paper, scissors? ");
+        if (choice === "Rock") {
+            return "You choose Rock"
+        } else if (choice === "Paper") {
+            return "You choose Paper"
+        } else {
+            return "You choose Scissors"
+        }
+
+    console.log(getHumanChoice(choice));
+}
