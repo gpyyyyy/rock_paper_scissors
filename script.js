5cb1e87f8516215ab4e0072438ee566c38b90736
gpyyyyy <renaldo.steinbach079@gmail.com>
2025-07-19 19:39:14 +0200

    Created function for playRound

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
+humanScore = 0;
+computerScore = 0;
 
-
-function getComputerChoice () {
+function getComputerChoice() {
     const min = 1;
     const max = 3;
     const randomChoice = Math.floor(Math.random() * (max - min + 1)) + min;
-        if (randomChoice == 1) {
-            return "rock"
-        } else if (randomChoice == 2) {
-            return "scissor"
-        } else {
-            return "paper"
-        }   
+    if (randomChoice == 1) {
+        return "Rock"
+    } else if (randomChoice == 2) {
+        return "Scissor"
+    } else {
+        return "Paper"
+    }
 }
 
-function getHumanChoice () {
+function getHumanChoice() {
     const choice = prompt("Rock, paper, scissors? ");
-        if (choice === "Rock") {
-            return "You choose Rock"
-        } else if (choice === "Paper") {
-            return "You choose Paper"
-        } else {
-            return "You choose Scissors"
-        }
+    if (choice === "Rock") {
+        console.log("You choose Rock")
+    } else if (choice === "Paper") {
+        console.log("You choose Paper")
+    } else {
+        console.log("You choose Scissors")
+    }
 
-    console.log(getHumanChoice(choice));
+    return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "Rock" && computerChoice === "Scissor") {
+        console.log("Du hast gewonnen!")
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        console.log("Der Computer hat gewonnen")
+    } else if (humanChoice === "Rock" && computerChoice === "Rock") {
+        console.log("Es ist ein untentschieden!")
+    }
+
+        if (humanChoice === "Paper" && computerChoice === "Scissor") {
+        console.log("Du hast verloren!")
+    } else if (humanChoice === "Paper" && computerChoice === "Paper") {
+        console.log("Es ist ein untenschieden")
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log("Du hast gewonnen!")
+    }
+
+        if (humanChoice === "Scissor" && computerChoice === "Scissor") {
+        console.log("Es ist ein untentschieden!")
+    } else if (humanChoice === "Scissor" && computerChoice === "Paper") {
+        console.log("Du hast gewonnen!")
+    } else if (humanChoice === "Scissor" && computerChoice === "Rock") {
+        console.log("Du hast verloren!")
+    }
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
